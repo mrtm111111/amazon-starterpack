33609f479816ecc7de76db1b6751ac9ae34a1d0a
mrtm111111 <tmgamig69@gmail.com>
2023-10-13 21:00:54 +0530

    shop_website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Hiro from "./components/Hero/Hiro";
+import Products from "./components/Products/Products";
+import Slider from "./components/Slider/Slider";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Virtual from "./components/Virtual/Virtual";
+import Header from "./components/header/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <span>Subscribe Zainkeepscode</span>
+      <Header/>
+      <Hiro/>
+      <Slider/>
+      <Virtual/>
+      <Products/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
